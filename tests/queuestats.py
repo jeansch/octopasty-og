bb4494143fbe3c9fbddea7eaadaada5ccf09f332
Jean Schurger <jean@schurger.org>
2011-02-09 12:34:39 -0500

    Added some real clients

---
+++ tests/queuestats.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+from Asterisk import Manager
+
+
+if __name__ == '__main__':
+    manager = Manager.Manager(('localhost', 4321),
+                              'plop', "plop",
+                              listen_events=False)
+    print manager.QueueStatus()
