98fba8dae7d0433b2c7ded11ef96c0252f290d0f
Jean Schurger <jean@schurger.org>
2011-03-04 09:49:30 -0500

    Fixed default comportment with user dosn't tell if he wants the events

--- octopasty/internal.py
+++ octopasty/internal.py
@@ -46,7 +46,7 @@ def handle_action(self, packet):
         auth_user(self, packet.emiter, packet.locked, login.get('username'),
                   login.get('secret'),
                   login.get('events') and \
-                  login.get('events').lower() == 'on' or False)
+                  login.get('events').lower() != 'off' or False)
 
     if action.name.lower() == 'logoff':
         logoff_user(self, packet)
